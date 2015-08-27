256d3a67874c1429646a33c564ae8412a50f8039
Barry Bankhead <bcbankhead@gmail.com>
2015-08-27 10:51:24 -0600

    added routing and partials

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,1 +1,18 @@
 var app = angular.module("persist", ['ngRoute']);
+
+app.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/posts.html',
+        controller: 'postController'
+      })
+      .otherwise({
+        templateUrl: 'partials/home.html',
+        controller: 'HomeController'
+      });
+
+
+      //use the html5 history api
+      $locationProvider.html5Mode(true);
+
+});
---
+++ javascripts/controllers.js
@@ -0,0 +1,8 @@
+app.controller('persistent',[ '$scope', '$http', function($scope,$http){
+  $scope.message = "DAMN Son..."
+}])
+
+
+app.controller('postController',[ '$scope', '$http', function($scope,$http){
+  $scope.message = "DAMN Son where'd you find these posts?"
+}])
